a12ad377e1e323f02f95f03fbfcf6b40c707c00c
dr-alberto <delrio.alberto@protonmail.com>
2020-07-20 09:59:31 +0200

    Change settings

--- web3auth/settings.py
+++ web3auth/settings.py
@@ -8,14 +8,14 @@ class AppSettings(object):
         Field on the User model, which has ethereum address to check against.
         This allows you to store it somewhere in arbitrary place other than just username.
         """
-        return getattr(django_settings, 'WEB3AUTH_USER_ADDRESS_FIELD', 'username')
+        return getattr(django_settings, 'WEB3AUTH_USER_ADDRESS_FIELD', 'address')
 
     @property
     def WEB3AUTH_USER_SIGNUP_FIELDS(self):
         """
         Specifies field to be used in signup form for a new User model
         """
-        return getattr(django_settings, "WEB3AUTH_USER_SIGNUP_FIELDS", ['email'])
+        return getattr(django_settings, "WEB3AUTH_USER_SIGNUP_FIELDS", ['username'])
 
     @property
     def WEB3AUTH_SIGNUP_ENABLED(self):
